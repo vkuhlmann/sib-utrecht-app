1be6d12a09d1817757c252d89f3a6a5dabf64f72
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2023-08-31 17:51:46 +0200

    Add indicator of event registration status

--- flutter/sib_utrecht_app/lib/activities.dart
+++ flutter/sib_utrecht_app/lib/activities.dart
@@ -7,7 +7,16 @@ class ActivityView extends StatefulWidget {
   final DateTime start;
   final DateTime end;
 
-  ActivityView({Key? key, required this.activity}) :
+  final bool isParticipating;
+  final ValueSetter<bool> setParticipating;
+
+  ActivityView(
+    {
+      Key? key, required this.activity,
+      required this.isParticipating,
+      required this.setParticipating
+    }
+  ) :
     start=DateTime.parse('${activity["event_start"]}Z').toLocal(),
     end=DateTime.parse('${activity["event_end"]}Z').toLocal(),
     super(key: key);
@@ -49,8 +58,8 @@ class _ActivityViewState extends State<ActivityView> {
               alignment: Alignment.center,
               padding: const EdgeInsets.all(10),
               margin: const EdgeInsets.all(5),
-              child: Text('${widget.start.day}'),
               color: Colors.lightBlueAccent,
+              child: Text('${widget.start.day}'),
             ),
             Container(
               alignment: Alignment.center,
@@ -68,6 +77,25 @@ class _ActivityViewState extends State<ActivityView> {
                 padding: const EdgeInsets.all(10),
                 margin: const EdgeInsets.all(5),
                 child: Text(widget.activity["event_name"])
+              )
+            ),
+            Container(alignment: Alignment.center,
+              // child: IconButton(
+              //   icon: const Icon(Icons.add),
+              //   onPressed: () {
+              //     // Navigator.push(
+              //     //   context,
+              //     //   MaterialPageRoute(builder: (context) => ActivityView(activity: widget.activity)),
+              //     // );
+              //     // Navigator.pushNamed(context, '/activity', arguments: widget.activity);
+              //   },
+              // )
+              child: Checkbox(
+                value: widget.isParticipating,
+                onChanged: (value) {
+                  print("Checkbox changed to $value");
+                  widget.setParticipating(value!);
+                }, 
               )
             ),
             Container(alignment: Alignment.centerLeft,
@@ -93,6 +121,27 @@ class _ActivityViewState extends State<ActivityView> {
   }
 }
 
+// class ActivityManager extends InheritedWidget {
+//   late Future<Map> events;
+//   late List<int> bookedEvents;
+
+//   ActivityManager({super.key, required super.child});
+
+//   @override
+//   void initState() {
+
+//   }
+
+//   static ActivityManager? of(BuildContext context) {
+//     return context.dependOnInheritedWidgetOfExactType<ActivityManager>();
+//   }
+
+//   @override
+//   bool updateShouldNotify(ActivityManager oldWidget) {
+//     return bookedEvents != oldWidget.bookedEvents;
+//   }
+// }
+
 class ActivitiesPage extends StatefulWidget {
   const ActivitiesPage({Key? key}) : super(key: key);
 
@@ -102,32 +151,72 @@ class ActivitiesPage extends StatefulWidget {
 
 class _ActivitiesPageState extends State<ActivitiesPage> {
   late Future<Map> _apiResult;
+  late Future<Set<int>> _bookedEvents;
   late Future<String>? _debugOutput;
 
   @override
   void initState() {
     super.initState();
-    _apiResult = APIConnector().get("events");
+    
+    var apiConnector = APIConnector();
+
+    _apiResult = apiConnector.get("events");
 
     _debugOutput = _apiResult.then((value) {
       const encoder = JsonEncoder.withIndent("    ");
       return encoder.convert(value);
     });
+
+    _bookedEvents = apiConnector.get("my-bookings").then((value) {
+      Set<int> events =
+        (value["data"]["bookings"] as List<dynamic>)
+        .where((v) => v["booking"]["status"] == "approved")
+        .map<int>((e) => e["event"]["event_id"])
+        .toSet();
+        return events;
+    });
+  }
+
+  void setEventRegistration(int eventId, bool value) {
+    print("Setting registration for event $eventId to $value");
+    // var apiConnector = APIConnector();
+    // apiConnector.post("bookings", {"event_id": eventId}).then((value) {
+    //   print("Registered for event $eventId");
+    //   setState(() {
+    //     _bookedEvents = apiConnector.get("my-bookings").then((value) {
+    //       List<int> events =
+    //         (value["data"]["bookings"] as List<dynamic>)
+    //         .where((v) => v["booking"]["status"] == "approved")
+    //         .map<int>((e) => e["event"]["event_id"])
+    //         .toList();
+    //         return events;
+    //     });
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-        future: _apiResult,
+    return Column(children: [
+    FutureBuilder<(Map, Set<int>)>(
+        future: Future.wait([_apiResult, _bookedEvents])
+        .then((value) => (value[0] as Map, value[1] as Set<int>)),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             // return Text(jsonEncode(snapshot.data!["data"]["events"]));
+            var (events_response, booked_events) = snapshot.data!;
+
 
             return Column(
-                children: snapshot.data!["data"]["events"]
+                children: events_response["data"]["events"]
                     // .map<Widget>((e) => Text(jsonEncode(e)))
                     // .map<Widget>((e) => Text("Test"))
-                    .map<Widget>((e) => ActivityView(activity: e))
+                    .map<Widget>((e) => ActivityView(
+                      activity: e,
+                      isParticipating: booked_events.contains(e["event_id"]),
+                      setParticipating:
+                        (value) => setEventRegistration(e["event_id"], value)
+                    ))
                     .toList()
                 // const <Widget>[
                 //   Text("Activity 1"),
@@ -140,7 +229,19 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
             return Text("${snapshot.error}");
           }
           return const CircularProgressIndicator();
-        });
+        }),
+        FutureBuilder<Set<int>>(
+          future: _bookedEvents,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(jsonEncode(snapshot.data!.toList()));
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return const CircularProgressIndicator();
+          }
+        ),
+    ]);
 
     // return Column(children: const <Widget>[
     //   Text("Activity 1"),
