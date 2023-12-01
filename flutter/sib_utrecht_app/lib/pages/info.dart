67f40e90bcb09709e02862c6f1e6722d1658bcb5
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2023-12-01 15:19:31 +0100

    Bump version to 0.2.0

--- flutter/sib_utrecht_app/lib/pages/info.dart
+++ flutter/sib_utrecht_app/lib/pages/info.dart
@@ -86,7 +86,7 @@ class _InfoPageState extends State<InfoPage> {
                                   showAboutDialog(
                                       context: context,
                                       applicationName: "SIB-Utrecht",
-                                      applicationVersion: "0.1.6");
+                                      applicationVersion: "0.2.0");
                                 },
                                 child: ListTile(title: Text(
                                     // "Over app"
